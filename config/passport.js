7977213e2a9a1a9d7c9565ec72a0b62cfebab8d8
Rohit das <devrohith0@gmail.com>
2024-10-15 19:38:13 +0530

    final code1

--- config/passport.js
+++ config/passport.js
@@ -21,7 +21,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: 'http://roshow.shop',
     passReqToCallback: true
   },
   async (req, accessToken, refreshToken, profile, done) => {
